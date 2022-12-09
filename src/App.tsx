184d2216004890aaba73c5aee74af68a6b43094a
AlaaEl-DinAhmed <alaaeldin.muhammad@gmail.com>
2022-12-09 22:02:52 +0200

    Add stack navigator

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,25 @@
 import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Platform, SafeAreaView, StyleSheet } from 'react-native';
+import { UserDetails } from './components/user-details/UserDetails';
 import { Home } from './screens/Home/Home';
 
 const queryClient = new QueryClient();
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <QueryClientProvider client={queryClient}>
+    <QueryClientProvider client={queryClient}>
+      <NavigationContainer>
         <SafeAreaView style={styles.droidSafeArea}>
-          <Home />
+          <Stack.Navigator initialRouteName="Home">
+            <Stack.Screen name="Home" component={Home} />
+            <Stack.Screen name="UserDetails" component={UserDetails} />
+          </Stack.Navigator>
         </SafeAreaView>
-      </QueryClientProvider>
-    </NavigationContainer>
+      </NavigationContainer>
+    </QueryClientProvider>
   );
 }
 
